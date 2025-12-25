5a005c815df6c4a44c9e62d67d3de47c3f103a9f
Saxv <vyragootoglobal@gmail.com>
2025-12-25 19:19:42 +0700

    Create indexer.py

---
+++ polygon/indexer.py
@@ -0,0 +1,14 @@
+"""
+Future indexer:
+- Scan Polygon blocks
+- Read tx input
+- Extract LAZV checkpoint
+"""
+
+def get_last_checkpoint():
+    # placeholder
+    return {
+        "chain": "LAZV",
+        "height": 1200,
+        "hash": "abc123..."
+    }
