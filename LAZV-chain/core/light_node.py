7a9e6425f9f673839b54e65a5fe7e2d8905ccef1
Saxv <vyragootoglobal@gmail.com>
2025-12-25 17:31:38 +0700

    Delete LAZV-chain/core/light_node.py

--- LAZV-chain/core/light_node.py
+++
@@ -1,9 +0,0 @@
-from polygon.checkpoint import read_last_checkpoint
-
-print("📱 LAZV Light Node")
-
-checkpoint = read_last_checkpoint()
-if checkpoint:
-    print("Last known chain state:", checkpoint)
-else:
-    print("No checkpoint found. Waiting for peers.")
